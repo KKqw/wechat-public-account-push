61507ddd52be64a89ba87374e1bb621ee0968e3f
KKqw <47825137+KKqw@users.noreply.github.com>
2023-02-17 16:43:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oB_ra6RZupbniXigPUeAPlz8cMho',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oQoXW0Vp2RFbMStdMrilYS6vr7kUv8OYHn0pyieedSE',
+      useTemplateId: '3i31NJqQ2dj2ldy5s_jfo_DdlyeuwoZ0ElrcG8n36LA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-12',
       festivals: [
