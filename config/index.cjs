e84b6533e99e496c0f0fd7b09f98adcb4c76d0f3
KKqw <47825137+KKqw@users.noreply.github.com>
2023-02-17 17:37:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oB_ra6RZupbniXigPUeAPlz8cMho',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '3UgUoKBswNsec3P3U7quzzHNeRhIqQcac4Rk45TqNI4',
+      useTemplateId: 'jiRyKv2Tycqa9g4jJW3GTw5xV6ujZXINmtnkX6HmVus',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-12',
       festivals: [
@@ -32,7 +32,7 @@ const USER_CONFIG = {
           type: '生日', name: 'KK', year: '1997', date: '03-12',
         },
         {
-          type: '*生日', name: 'KK', year: '1997', date: '03-12',
+          type: '*农历生日', name: 'KK', year: '1997', date: '03-12',
         },
        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
