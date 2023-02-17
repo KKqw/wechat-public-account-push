f2bcfde384cf369854dc6dfe56c17b7bfa23b126
KKqw <47825137+KKqw@users.noreply.github.com>
2023-02-17 17:40:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -34,7 +34,6 @@ const USER_CONFIG = {
         {
           type: '*农历生日', name: 'KK', year: '1997', date: '03-12',
         },
-       // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: 'Ta', year: '1996', date: '04-29',
         },
