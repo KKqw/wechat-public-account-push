fb6ccbc916bf8b65a8222db52e3bbba39bc38433
KKqw <47825137+KKqw@users.noreply.github.com>
2023-02-17 18:58:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -102,6 +102,17 @@ const USER_CONFIG = {
       ],
     },
     
+    {
+      // 想要发送的人的名字
+      name: '嘤嘤嘤的今日运势',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oB_ra6RZupbniXigPUeAPlz8cMho',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'QS0bz5o7hqSKC-i6HoEtmbvB3UOWifXbkwGrCacpdmk',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '04-29',
+    },
+    
   ],
 
 
