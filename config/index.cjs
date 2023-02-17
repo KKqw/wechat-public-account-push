e357b0c086268b8f1a61d32bb94a2c48c739bbfc
KKqw <47825137+KKqw@users.noreply.github.com>
2023-02-17 16:12:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	BG2FHHIaASyPLLr45j2YhlbkBvQVFlzs6gVdZkcf3jo',
+  CALLBACK_TEMPLATE_ID: 'BG2FHHIaASyPLLr45j2YhlbkBvQVFlzs6gVdZkcf3jo',
 
   CALLBACK_USERS: [
     {
